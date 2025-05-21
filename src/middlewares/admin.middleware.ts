d9e45874d547766df5c223adb543bbdbb188213d
CABKD <cheikhkane.diallo@gmail.com>
2025-05-21 16:19:27 +0200

    Backend API middlewares

---
+++ src/middlewares/admin.middleware.ts
@@ -0,0 +1,15 @@
+import { Response, NextFunction } from "express";
+import { AuthRequest } from "../types/auth-request";
+
+
+export const isAdmin = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+): void => {
+  if (req.user?.email === "admin@admin.com") {
+    return next();
+  }
+  res.status(403).json({ message: "Admin only" });
+};
+
